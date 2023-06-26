ab68e40470bad589f835a70f455c101e29b1ae52
developer <developerweb6@gmail.com>
2023-06-26 10:08:09 +0530

    Changes

--- src/components/pages/CreateInfluencer.jsx
+++ src/components/pages/CreateInfluencer.jsx
@@ -411,55 +411,47 @@ const CreateInfluencer = () => {
     };
 
     useEffect(() => {
-        if (Array.isArray(productName) && selectedRows.length > 0) {
+        if (Array.isArray(productName)) {
           setLoading(true);
-          const influencerIds = selectedRows.map(row => row.id);
-          const requests = productName.map(product =>
-            axios.post(API.BASE_URL + "product/url/", {
-              products: productIds.filter(Boolean).toString()
-            }, {
-              headers: {
-                Authorization: `Token ${token}`,
-              },
+          Promise.all(
+            productName?.map((product) => {
+              return axios
+                .post(API.BASE_URL + "product/url/", {
+                  products: productIds.filter(Boolean).toString()
+                }, {
+                  headers: {
+                    Authorization: `Token ${token}`,
+                  },
+                })
+                .then((response) => {
+                  console.log("Response 1", response);
+                  const influencerIds = response.data.product_details.map(
+                    (product) => product.influencer_id
+                  );
+                  setProdInfluId(influencerIds);
+                  setProductUrl(response.data.product_url);
+                  const matchedRows = selectedRows.filter(
+                    (row) => influencerIds.includes(row.id)
+                  );
+      
+                  console.log("matchedRows", matchedRows)
+                  if(showInfluList == false) {
+                    if (matchedRows.length > 0) {
+                        const matchedProductDetails = response.data.product_details.filter(
+                          (product) => matchedRows.some((row) => row.id === product.influencer_id)
+                        );
+                        setProductDetails(matchedProductDetails);
+                      } else {
+                        // Show toast error here
+                        toast.error("No matching rows found.");
+                      }
+                  }
+                })
+                .catch((error) => console.log(error));
             })
-          );
-      
-          Promise.all(requests)
-            .then(responses => {
-              const productDetails = [];
-              const matchedRows = [];
-      
-              responses.forEach((response, index) => {
-                console.log("Response", index, response);
-                const currentProductDetails = response.data.product_details;
-                const matchingProductDetails = currentProductDetails.filter(product =>
-                  influencerIds.includes(product.influencer_id)
-                );
-      
-                if (matchingProductDetails.length > 0) {
-                  matchedRows.push(...matchingProductDetails);
-                  productDetails.push(...matchingProductDetails);
-                }
-              });
-      
-              if (matchedRows.length > 0) {
-                setProdInfluId(matchedRows.map(row => row.influencer_id));
-                setProductUrl(matchedRows[0].product_url);
-                setProductDetails(productDetails);
-              } else {
-                toast.error("No matching rows found.");
-              }
-            })
-            .catch(error => {
-              console.log(error);
-              toast.error("An error occurred while fetching data.");
-            })
-            .finally(() => setLoading(false));
+          ).finally(() => setLoading(false));
         }
-      }, [productName, selectedRows, token]);
-      
-      
-      
+    }, [productName, selectedRows, token]); 
 
     const handleCheckboxChange = (event, row, id) => {
         const checked = event.target.checked;
